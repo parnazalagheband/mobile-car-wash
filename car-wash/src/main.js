a4b7e504ac1b9edb87033859b47253ba2c156d52
parnaz <parnazalagheband@gmail.com>
2023-11-06 12:11:57 +0330

    feat: first initial

---
+++ car-wash/src/main.js
@@ -0,0 +1,19 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(BootstrapVue3)
+
+app.mount('#app')
